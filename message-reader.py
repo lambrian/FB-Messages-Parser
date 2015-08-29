a5bea4b2f9434790f053fcf572041bed0b3b69be
Brian Lam <brianlam@stanford.edu>
2015-08-28 23:42:27 -0700

    look for specific thread

--- message-reader.py
+++ message-reader.py
@@ -16,9 +16,13 @@ def process_message (f, curr_message_index):
     len(MESSAGE_AUTHOR_HEADER))
     body_index = (f.find (MESSAGE_BODY_HEADER, curr_message_index) + len
     (MESSAGE_BODY_HEADER))
+    date_index = (f.find (MESSAGE_DATE_HEADER, curr_message_index) + len
+    (MESSAGE_DATE_HEADER))
     
     author = f[author_index:f.find ('<', author_index)]
     body = f[body_index:f.find ('<', body_index)]
+    date = f[date_index:f.find ('<', date_index)]
+    print author, body, date
 
 '''
 Given the index of current thread index in file and index of next thread,
@@ -36,10 +40,11 @@ Processes all message threads
 def process_threads (f):
     curr_thread_index = f.find(THREAD_HEADER)
     while curr_thread_index > -1:
-        print f[curr_thread_index: f.find('<', curr_thread_index + 1)]
-        next
-        next_thread_index = f.find(THREAD_HEADER, curr_thread_index + 1)
-        process_thread_messages (f, curr_thread_index, next_thread_index)
+        curr_thread_index = curr_thread_index + len (THREAD_HEADER)
+        thread_participants = f[curr_thread_index:f.find('<', curr_thread_index)]
+        next_thread_index = f.find(THREAD_HEADER, curr_thread_index)
+        if thread_participants == 'Jason Teplitz, Brian Lam':
+            process_thread_messages (f, curr_thread_index, next_thread_index)
         curr_thread_index = next_thread_index
 
 def main():
